1224b0a2f9b472bd8bb8211faad3ca3f7e11a5f2
alvaropfn <alvaropfn@gmail.com>
2021-11-05 07:19:12 -0300

    maping enviroment successfully

--- warriorjs/alvaropfn-baby-steps/Player.js
+++ warriorjs/alvaropfn-baby-steps/Player.js
@@ -12,27 +12,45 @@ const STUFF = {
 };
 
 class Player {
-  constructor() {
-    this.warrior = {};
-    this.oldHP = 20;
-    this.newHP = this.oldHP;
-    this.nearby = {
-      up: "",
-      right: "",
-      down: "",
-      left: "",
-    };
-  }
-
   playTurn(warrior) {
-    this.warrior = warrior;
-    this.actions.move(DIRECTIONS.right);
+    this.context = this.context
+      ? this.context
+      : new Context(new Stand(), warrior);
 
     // Cool code goes here.
-    this.print(this.actions.see());
+    // this.print(this.actions.see());
+    // this.context.doStuff()
+  }
+}
+/**
+ * Context holds all information about the actual enviroment of our warrior
+ */
+class Context {
+  constructor(state, warrior) {
+    this.warrior = warrior;
+
+    this.oldHP = 20;
+    this.newHP = this.oldHP;
+    this.map = this.seeAround();
+    this.print(this.map);
+
+    // this.print(this.actions.see(DIRECTIONS.left));
+
+    this.setState(state);
   }
 
-  print = (msg) => this.warrior.think(msg.getLocation());
+  setState(state) {
+    this.state = state;
+    this.state.setContext(this);
+  }
+
+  doStuff() {
+    this.warrior.seeAround();
+
+    this.state.doStuff();
+  }
+
+  print = (msg) => this.warrior.think(msg);
 
   actions = {
     move: (direction = DIRECTIONS.right) => this.warrior.walk(direction),
@@ -41,57 +59,35 @@ class Player {
   };
 
   seeAround() {
-    const up = STUFF.empty;
-    const right = STUFF.empty;
-    const down = STUFF.empty;
-    const left = STUFF.empty;
+    let up = STUFF.empty;
+    let right = STUFF.empty;
+    let down = STUFF.empty;
+    let left = STUFF.empty;
 
     // Map Enemys
-    up = this.actions.see(DIRECTIONS.up).isEnemy ? STUFF.enemy : up;
-    right = this.actions.see(DIRECTIONS.right).isEnemy ? STUFF.enemy : right;
-    down = this.actions.see(DIRECTIONS.down).isEnemy ? STUFF.enemy : down;
-    left = this.actions.see(DIRECTIONS.left).isEnemy ? STUFF.enemy : left;
+    up = this.actions.see(DIRECTIONS.up).isUnit() ? STUFF.enemy : up;
+    right = this.actions.see(DIRECTIONS.right).isUnit() ? STUFF.enemy : right;
+    down = this.actions.see(DIRECTIONS.down).isUnit() ? STUFF.enemy : down;
+    left = this.actions.see(DIRECTIONS.left).isUnit() ? STUFF.enemy : left;
 
     // Map Walls
-    up = this.actions.see(DIRECTIONS.up).isWall ? STUFF.wall : up;
-    right = this.actions.see(DIRECTIONS.right).isWall ? STUFF.wall : right;
-    down = this.actions.see(DIRECTIONS.down).isWall ? STUFF.wall : down;
-    left = this.actions.see(DIRECTIONS.left).isWall ? STUFF.wall : left;
+    up = this.actions.see(DIRECTIONS.up).isWall() ? STUFF.wall : up;
+    right = this.actions.see(DIRECTIONS.right).isWall() ? STUFF.wall : right;
+    down = this.actions.see(DIRECTIONS.down).isWall() ? STUFF.wall : down;
+    left = this.actions.see(DIRECTIONS.left).isWall() ? STUFF.wall : left;
+
+    // Map Empty
+    up = this.actions.see(DIRECTIONS.up).isEmpty() ? STUFF.empty : up;
+    right = this.actions.see(DIRECTIONS.right).isEmpty() ? STUFF.empty : right;
+    down = this.actions.see(DIRECTIONS.down).isEmpty() ? STUFF.empty : down;
+    left = this.actions.see(DIRECTIONS.left).isEmpty() ? STUFF.empty : left;
 
     return { right, down, left, up };
   }
 }
-/**
- * Context holds all information about the actual enviroment of our warrior
- */
-class Context {
-  constructor(state, warrior) {
-    this.changeState(state);
-    this.warrior = warrior;
-
-    this.oldHP = 20;
-    this.newHP = this.oldHP;
-  }
-
-  changeState(state) {
-    this.state = state;
-    this.state.SetContext(this);
-  }
-
-  setState(state) {
-    this.state = state;
-    this.state.SetContext(this);
-  }
-
-  doStuff() {
-    this.warrior.seeAround();
-
-    this.state.doStuff();
-  }
-}
 // States
 class AbstractState {
-  setState(context) {
+  setContext(context) {
     this.context = context;
   }
 
